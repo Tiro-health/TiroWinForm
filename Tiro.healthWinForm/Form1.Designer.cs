bac8d14fb74511eca331e4ea676a9142fff54700
Axel Vanraes <axelvanraes@gmail.com>
2021-11-17 14:49:07 +0100

    Added fields for new params. Submit button

--- Tiro.healthWinForm/Form1.Designer.cs
+++ Tiro.healthWinForm/Form1.Designer.cs
@@ -31,19 +31,26 @@ namespace Tiro.healthWinForm
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
-            this.apiKeyText = new System.Windows.Forms.TextBox();
+            this.apiKey = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.ageText = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.sexText = new System.Windows.Forms.TextBox();
+            this.sex = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.JSONTextBox = new System.Windows.Forms.RichTextBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.XMLTextBox = new System.Windows.Forms.RichTextBox();
+            this.template = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.patient = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.encounter = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -57,17 +64,17 @@ namespace Tiro.healthWinForm
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.webView21.Location = new System.Drawing.Point(0, 0);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(1759, 629);
+            this.webView21.Size = new System.Drawing.Size(1762, 477);
             this.webView21.Source = new System.Uri("http://10.37.129.2:3000/embedded/templates", System.UriKind.Absolute);
             this.webView21.TabIndex = 0;
             this.webView21.ZoomFactor = 1D;
             // 
-            // apiKeyText
+            // apiKey
             // 
-            this.apiKeyText.Location = new System.Drawing.Point(106, 16);
-            this.apiKeyText.Name = "apiKeyText";
-            this.apiKeyText.Size = new System.Drawing.Size(642, 31);
-            this.apiKeyText.TabIndex = 1;
+            this.apiKey.Location = new System.Drawing.Point(106, 16);
+            this.apiKey.Name = "apiKey";
+            this.apiKey.Size = new System.Drawing.Size(642, 31);
+            this.apiKey.TabIndex = 1;
             // 
             // label1
             // 
@@ -87,12 +94,12 @@ namespace Tiro.healthWinForm
             this.label2.TabIndex = 4;
             this.label2.Text = "Age";
             // 
-            // ageText
+            // age
             // 
-            this.ageText.Location = new System.Drawing.Point(106, 53);
-            this.ageText.Name = "ageText";
-            this.ageText.Size = new System.Drawing.Size(90, 31);
-            this.ageText.TabIndex = 3;
+            this.age.Location = new System.Drawing.Point(106, 53);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(90, 31);
+            this.age.TabIndex = 3;
             // 
             // label3
             // 
@@ -103,16 +110,16 @@ namespace Tiro.healthWinForm
             this.label3.TabIndex = 6;
             this.label3.Text = "Sex";
             // 
-            // sexText
+            // sex
             // 
-            this.sexText.Location = new System.Drawing.Point(106, 91);
-            this.sexText.Name = "sexText";
-            this.sexText.Size = new System.Drawing.Size(90, 31);
-            this.sexText.TabIndex = 5;
+            this.sex.Location = new System.Drawing.Point(106, 91);
+            this.sex.Name = "sex";
+            this.sex.Size = new System.Drawing.Size(90, 31);
+            this.sex.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(767, 91);
+            this.button1.Location = new System.Drawing.Point(1308, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 31);
             this.button1.TabIndex = 7;
@@ -141,6 +148,17 @@ namespace Tiro.healthWinForm
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "NoteEditor";
             this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1341, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // tabPage2
             // 
@@ -180,17 +198,73 @@ namespace Tiro.healthWinForm
             this.XMLTextBox.TabIndex = 0;
             this.XMLTextBox.Text = "";
             // 
+            // template
+            // 
+            this.template.Location = new System.Drawing.Point(897, 16);
+            this.template.Name = "template";
+            this.template.Size = new System.Drawing.Size(344, 31);
+            this.template.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(783, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "template";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(783, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "patient";
+            // 
+            // patient
+            // 
+            this.patient.Location = new System.Drawing.Point(897, 53);
+            this.patient.Name = "patient";
+            this.patient.Size = new System.Drawing.Size(344, 31);
+            this.patient.TabIndex = 12;
+            this.patient.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(783, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "encounter";
+            // 
+            // encounter
+            // 
+            this.encounter.Location = new System.Drawing.Point(897, 91);
+            this.encounter.Name = "encounter";
+            this.encounter.Size = new System.Drawing.Size(344, 31);
+            this.encounter.TabIndex = 14;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1773, 817);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.encounter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.patient);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.template);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.sexText);
+            this.Controls.Add(this.sex);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.ageText);
+            this.Controls.Add(this.age);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.apiKeyText);
+            this.Controls.Add(this.apiKey);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Tiro.health - WebView2 Embedded NoteEditor";
@@ -207,12 +281,12 @@ namespace Tiro.healthWinForm
         #endregion
 
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
-        private System.Windows.Forms.TextBox apiKeyText;
+        private System.Windows.Forms.TextBox apiKey;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox ageText;
+        private System.Windows.Forms.TextBox age;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox sexText;
+        private System.Windows.Forms.TextBox sex;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
@@ -220,6 +294,13 @@ namespace Tiro.healthWinForm
         private System.Windows.Forms.RichTextBox JSONTextBox;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.RichTextBox XMLTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox template;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox patient;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox encounter;
     }
 }
 
--- Tiro.healthWinForm/Form1.cs
+++ Tiro.healthWinForm/Form1.cs
@@ -26,7 +26,7 @@ namespace Tiro.healthWinForm
 
         private void Form_Resize(object sender, EventArgs e)
         {
-            webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location);
+            webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location) - new Size(0, 100);
             tabControl1.Size = this.ClientSize - new System.Drawing.Size(tabControl1.Location);
         }
 
@@ -37,9 +37,41 @@ namespace Tiro.healthWinForm
 
         }
 
+        private void requestSubmit()
+        {
+            this.webView21.CoreWebView2.PostWebMessageAsJson("{\"action\": \"submit\"}");
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            this.webView21.Source = new System.Uri("http://write.tiro.health/embedded/templates?age=" + this.ageText.Text + "&sex=" + this.sexText.Text + "&apiKey=" + this.apiKeyText.Text);
+            var uri = "https://write.tiro.health/embedded/notes?";
+            if(this.age.TextLength > 0)
+            {
+                uri += "age=" + this.age.Text+"&";
+            }
+            if(this.sex.TextLength > 0)
+            {
+                uri += "sex=" + this.sex.Text+"&";
+            }
+            if(this.apiKey.TextLength > 0)
+            {
+                uri += "apiKey=" + this.apiKey.Text+"&";
+            }
+            if(this.patient.TextLength > 0)
+            {
+                uri += "patient=" + this.patient.Text+"&";
+            }
+            if(this.encounter.TextLength > 0)
+            {
+                uri += "encounter=" + this.encounter.Text+"&";
+            }
+            if(this.template.TextLength > 0)
+            {
+                uri += "template=" + this.template.Text+"&";
+            }
+
+            Uri uri1 = new System.Uri(uri);
+            this.webView21.Source = uri1;
         }
 
         public string FhirJSONDocument {
@@ -65,8 +97,23 @@ namespace Tiro.healthWinForm
             }
 
         }
-    }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.requestSubmit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    // This class should be added to your project. The form here is used to display the received variables
     [ClassInterface(ClassInterfaceType.AutoDual)]
     [ComVisible(true)]
     public class Bridge
@@ -90,17 +137,26 @@ namespace Tiro.healthWinForm
             }
             return serializer.SerializeToString(this._response);
         }
+       
         public void setFhirDocument(string fhirJsonDocument)
         {
             var parser = new FhirJsonParser(new ParserSettings
             {
-                AcceptUnknownMembers = true,
+                AcceptUnknownMembers = false,
                 AllowUnrecognizedEnums = true
             });
 
             try
             {
                 this._response = parser.Parse<Bundle>(fhirJsonDocument);
+                var composition =(Composition)this._response.Entry.First().Resource;
+                foreach(var section in composition.Section)
+                {
+                    var code = section.Code.Coding.Find((c) => c.System == "http://tce.tiro.health/fhir/DocumentSections").Code;
+                    Console.WriteLine(code);
+                    Console.WriteLine(section.Title);
+                    Console.WriteLine(section.Text.Div);
+                }
                 this._form.FhirJSONDocument = this._response.ToJson(new FhirJsonSerializationSettings()
                 {Pretty = true}
                     );
